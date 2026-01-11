a3154ca70744ab3ab0537757efdfb02dcdb9756b
jbilodea <74976277+jbilodea@users.noreply.github.com>
2026-01-11 15:03:23 -0500

    Update app version to 1.2.6

--- Canadian_Hyundai_Bluelink/Drivers/BluelinkDriver.groovy
+++ Canadian_Hyundai_Bluelink/Drivers/BluelinkDriver.groovy
@@ -23,10 +23,10 @@
  *  12/29/25 - Minor improvements for Cloudflare compatibility (v1.2.2)
  */
 
-String appVersion()   { return "1.2.2" }
+String appVersion()   { return "1.2.6" }
 def setVersion(){
     state.name = "Hyundai Bluelink Driver"
-    state.version = "1.2.2"
+    state.version = "1.2.6"
 }
 
 metadata {
